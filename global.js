9a33f9beb23947730ad7e6adf68e5ff52f84561b
Scott Spencer <scottspencer2@gmail.com>
2013-01-05 16:34:04 -0500

    getting close to finishing the site!

--- global.js
+++ global.js
@@ -1,12 +1,27 @@
 $(function() {
 	$(".branchBubble").click(function(e) {
-		$(".informationBoxWrapper").removeClass("viewing");
-		$("#" + $(this).attr("id") + "Text").addClass("viewing");
+		var id = "#" + $(this).attr("id") + "Text";
+		if($(".informationBoxWrapper").hasClass("viewing")) {
+			showText(id);
+		} else {
+			$(".formingCloud").addClass("inPosition");
+			setTimeout(function() {
+				$(".formingCloud").hide();
+				$(".formingCloud").removeClass("inPosition")
+				showText(id);
+			}, 1000);
+		}
 		e.preventDefault();
 	});
 	
 	$("#logo").click(function(e) {
 		$(".informationBoxWrapper").removeClass("viewing");
+		$(".formingCloud").show();
 		e.preventDefault();
 	});
 });
+
+function showText(id) {
+	$(".informationBoxWrapper").removeClass("viewing");
+	$(id).addClass("viewing");
+}
